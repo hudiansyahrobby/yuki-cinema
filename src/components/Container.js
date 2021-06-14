4a85d67551ffe195583cbfb64f5c774426885e35
hudiansyahrobby <hudiansyahrobby7@gmail.com>
2021-06-14 19:13:57 +0800

    push

--- src/components/Container.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Container({ children }) {
-  return <div className='max-w-screen-xl mx-4'>{children}</div>;
-}
--- src/helpers/generatePublicPath.js
+++
@@ -1,3 +0,0 @@
-export const generatePublicPath = (filename) => {
-  return `http://localhost:5000/${filename}`;
-};
